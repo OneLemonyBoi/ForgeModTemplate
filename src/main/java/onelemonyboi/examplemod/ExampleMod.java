e5a52d0516b67f3e029c993d7efd33f87bad1923
OneLemonyBoi <50998649+OneLemonyBoi@users.noreply.github.com>
2021-07-04 17:14:05 -0700

    Update Template

--- src/main/java/onelemonyboi/examplemod/ExampleMod.java
+++ src/main/java/onelemonyboi/examplemod/ExampleMod.java
@@ -13,46 +13,27 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
+@Mod(ExampleMod.MOD_ID)
 public class ExampleMod
 {
     // Directly reference a log4j logger.
 
-    public static final String MODID = "examplemod";
+    public static final String MOD_ID = "examplemod";
 
     private static final Logger LOGGER = LogManager.getLogger();
 
     public ExampleMod() {
-        // Register the setup method for modloading
+        ModRegistry.register();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) {
+
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
-    }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
 
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-
-        }
     }
 }
